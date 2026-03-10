e6736caa46eb68e18b446cec5d1a3452c8ff27cd
Naim <naim.proto20@gmail.com>
2026-03-10 11:53:27 +0100

    base

--- main.go
+++ main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
-func readFile(filename string) {
+
+func readFile(filename string) ([]string, error) {
 	file, err := os.Open(filename)
     if err != nil {
         return nil, err
     }
     defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("test")
+		fmt.Println("Usage: go run . test.txt")
 		os.Exit(1)
 	}
 
-	ant, err := readFile(os.Args[1])
+	lines, err := readFile(os.Args[1])
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("ERROR:", err)
 		return
+	}
+
+	for _, line := range lines {
+		fmt.Println(line)
 	}
 }
